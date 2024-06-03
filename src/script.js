c0b5de9c28619f1eb8573eaeb0dbceb7a8b9f2f9
kayvan1502 <75264733+kayvan1502@users.noreply.github.com>
2024-06-03 16:16:38 +0000

    Creating an external script file

---
+++ src/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+
+    let currentValue = "";
+
+    function evaluateResult() {
+        const convertedValue = currentValue
+        .replace("×","*")
+        .replace("÷","/")
+        .replace('%','*0.01');
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        //console.log(result);
+        display.value = currentValue;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
+
+            if (value == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+            } else if(value == "="){
+                evaluateResult();
+            } else {
+                currentValue += value;
+                display.value = currentValue;
+            }
+
+        })
+    }
+
+})
